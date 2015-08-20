4d99a67be92ca4e842be2690bd49d1634d16150e
Prashant Singh Pawar <prashantpawar@gmail.com>
2015-08-20 01:19:41 +0000

    Adding an echo message whenever HTTPQuery.js is being built

--- mkbrowser.sh
+++ mkbrowser.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "Building HTTPQuery.js"
 mv js/HTTPQuery.js js/HTTPQuery-node.js
 sed 's/require("request")/require("browser-request")/' js/HTTPQuery-node.js >js/HTTPQuery.js
 browserify -r ./js/Contract:Contract -r ./js/Transaction:Transaction -r ./js/Solidity:Solidity -r ./js/Faucet:claimEther > api.js
