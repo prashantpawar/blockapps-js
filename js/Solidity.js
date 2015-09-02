2bf2ddc2e72a457bbd8a9bab78e543ed24c3ea0e
Ryan Reich <ryan.reich@gmail.com>
2015-09-02 11:51:30 -0400

    Rewrite Solidity.js with promises and including the rest of Contract.js.

--- js/Solidity.js
+++ js/Solidity.js
@@ -1,69 +1,54 @@
-var Contract = require("./Contract.js");
+var solc = require("./Routes.js").solc;
+var solidityType = require("./solidityType.js");
+var Account = require("./Account.js");
 var Transaction = require("./Transaction.js");
-var HTTPQuery = require("./HTTPQuery.js");
+var Storage = require("./Storage.js");
 
 module.exports = Solidity;
 
 function Solidity(code) {
-    if (this instanceof Solidity) {
-        this.code = code;
-        this.vmCode = undefined;
-//        this.abi = undefined;
-        this.symtab = undefined;
-
-        this.toContract = compileAndSubmit;
-        this.compile = compileSolidity;
-        this.submit = submitSolidity;
-    }
-    else {
-        return new Solidity(code);
-    }
-}
-
-// argObj = {
-//   apiURL:, fromAccount:, value:, gasPrice:, gasLimit:
-// }
-function compileAndSubmit(argObj, callback) {
-    function compileCallback(solidity) { 
-        submitSolidity.bind(this) (argObj, callback);
-    }
-    compileSolidity.bind(this)(argObj.apiURL, compileCallback.bind(this));
-}
-
-function submitSolidity(argObj, callback) {
-    argObj.toAccount = Contract();
-    argObj.data = this.vmCode;
-    var submitTX = Transaction(argObj);
-    submitTX.send(argObj.apiURL, contractCallback.bind(this));
-
-    function contractCallback(txResult) {
-        var contract = Contract({
-            address: txResult.contractsCreated[0], // Only handle one-contract code
- //           abi: this.abi,
-            symtab: this.symtab,
-        });
-        callback(contract);
-    }
-}
-
-function compileSolidity(apiURL, callback) {
-    function getSolc(solcResult) {
-        if (solcResult["contracts"].length != 1) {
-            console.log("Code must define one and only one contract");
-            return;
-        }
-        this.symtab = solcResult["xabis"];
-        //this.abi = solcResult["abis"][0]["abi"];
-        this.vmCode = solcResult["contracts"][0]["bin"];
-        if (typeof callback === "function") {
-            callback(this);
-        }
-    }
-
-    HTTPQuery({
-        "serverURI":apiURL,
-        "queryPath":"/solc",
-        "post":{"src":this.code},
-        "callback":getSolc.bind(this)
+    return solc(code).then(function(x) {
+        var result = Object.create(Solidity.prototype);
+        result.code = code;
+        result.vmCode = x.vmCode;
+        result.symTab = x.symTab;
+        return result;
+    }).catch(function(e) {
+        throw "Solidity(code): " + e;
     });
 }
+Solidity.prototype = {
+    "code" : "",
+    "vmCode" : null,
+    "symTab" : null,
+    "constructor" : Solidity,
+    "newContract" : SolContract
+};
+
+// txParams = {value, gasPrice, gasLimit, privkey}
+function SolContract(txParams) {
+    var solObj = this;
+    txParams.data = this.vmCode;
+    return Transaction(txParams)(null, txParams.privKey).then(function(txResponse){
+        if (txResponse.contractsCreated.length !== 1) {
+            throw "Solidity(code).newContract(_): " +
+                "code must create one and only one account";
+        }
+        var newAddr = txResponse.contractsCreated[0];
+        var result = Object.create(solObj);
+        var storage = new Storage(newAddr);
+        result.account = new Account(solidityType.newAddress().canonStr(newAddr));
+        result.state = {};
+        for (var sym in solObj.symTab) {
+            var symRow = solObj.symTab[sym];
+            if (symRow["atStorageKey"] === undefined &&
+                symRow["jstype"] !== "Function") { // user-defined types
+                continue;
+            }
+            result.state[sym] = solidityType(symRow).storage(storage);
+        }
+        return result;
+    }).catch(function(e) {
+        throw "Solidity.newContract(txParams): " + e;
+    });
+};
