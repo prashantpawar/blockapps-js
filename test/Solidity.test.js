eddf49c257472561dac78ac8195c1a8390766428
Ryan Reich <ryan.reich@gmail.com>
2015-09-30 02:36:54 -0400

    Bump package.js version, new bugfix release

--- test/Solidity.test.js
+++ test/Solidity.test.js
@@ -1,12 +1,50 @@
-//var Solidity = require("../js/Solidity.js")
-
-var chai = require("chai");
-var expect = chai.expect;
 describe("Solidity", function () {
-    beforeEach(function () {
-        
+    before(function() {
+        Solidity = lib.Solidity;
     });
-    it("should return a new Solidity object", function () {
-        expect([1,2,3].indexOf(5)).to.equal(-1);
+    beforeEach(function () {
+        pollevery = lib.polling.pollEveryMS;
+        timeout = lib.polling.pollTimeoutMS;
+    });
+    afterEach(function() {
+        lib.polling.pollEveryMS = pollevery;
+        lib.polling.pollTimeoutMS = timeout;
+        nock.cleanAll();
+    });
+
+    describe("the Solidity constructor", function() {
+        before(function() {
+            code = "                 \
+contract C {                             \n\
+  int x = -1;                            \n\
+  function C() {}                        \n\
+  function f(string s) returns (int) {}  \n\
+}                                        \
+";
+        });
+        beforeEach(function() {
+            blockapps.post("/solc").reply(200, {
+                "contracts" : [{"name" : "C", "bin" : "60006c"}],
+                "xabis" : { "C" : {"x" : {}, "f" : {} } }
+            }
+        });
+        it("accepts valid Solidity code, returning a Solidity object", function() {
+            return expect(Solidity(code)).to.eventually.be.fulfilled
+                .and.to.eventually.be.an.instanceOf(Solidity);
+        });
+        it("correctly assigns code and contract name"), function() {
+            var solCode = Solidity(code);
+            return Promise.join(
+                expect(solCode).to.eventually.have.property("code", code),
+                expect(solCode).to.eventually.have.property("name", "C"),
+                function(){}
+            );
+        });
+    });
+    describe("contract objects", function() {
+
+    });
+    describe("the 'attach' function", function() {
+        
     });
 });
